f422ba72f30cc3d481b756b044e779341254bf92
Opilane TTHK <opilane@tthk.ee>
2021-11-11 10:28:15 +0200

    datafromfile

---
+++ DataFromFile/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+
+namespace DataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ente your favorite colour : ");
+            string usercolor = Console.ReadLine();
+
+
+
+            
+
+        }
+
+        private static string[] ReadDataFromFile()
+        {
+           string directoryPath = @"C:\Users\opilane\Samples";
+           string filename = "Personality.txt";
+           string fullpath = $@"{directoryPath}\{filename}";
+
+           string[] datafrofile = File.ReadAllLines(fullpath);
+           return DataFromFile;
+
+        }
+           
+      private static string GetTheFileFromFileData(string userinput)
+        {
+            string[] datafromfile = ReadDataFromFile();
+            string result = "";
+            
+            foreach (string line in datafrofile)
+            {
+                if (line.Contains(userinput))
+                {
+                    Console.WriteLine(line);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sorry i didnt notrcognize the color");
+                    Console.ReadLine();
+                }
+
+
+            }
+
+
+        }
+        
+      
+    }
+}
---
+++ Horoscope/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+              Console.WriteLine(randomLineFromFile();
+            }
+
+            
+
+            private static string randomLineFromFile()
+            {
+              string directoryPath = @"C:\Users\opilane\Samples";
+              string filename = "Chuck.txt";
+              string fullpath = $@"{directoryPath}\{filename}";
+
+
+              string[] datafromfile = File.ReadAllLines(fullpath);
+              Random rnd = new Random();
+              Console.WriteLine(datafromfile[rnd.Next(datafromfile.Length)])
+                
+            }
+
+        }
+    }
+}
